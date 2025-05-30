733b9fb61fe2299b543ddf4aebbd5c73e5954c96
DiegoFJusto <diegofjusto@gmail.com>
2025-05-30 17:14:53 -0300

    feat: adiciona cluster ECS e banco RDS MySQL

--- lib/desafio-sr-infra-stack.ts
+++ lib/desafio-sr-infra-stack.ts
@@ -1,16 +1,51 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as rds from 'aws-cdk-lib/aws-rds';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 
 export class DesafioSrInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // VPC com 2 AZs
+    const vpc = new ec2.Vpc(this, 'DesafioVpc', {
+      maxAzs: 2,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'DesafioSrInfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Cluster ECS
+    const cluster = new ecs.Cluster(this, 'DesafioCluster', {
+      vpc,
+      clusterName: 'DesafioSrCluster',
+    });
+
+    // Secret para armazenar credenciais do banco
+    const dbCredentialsSecret = new secretsmanager.Secret(this, 'DBCredentialsSecret', {
+      secretName: 'DesafioDBCredentials',
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({ username: 'admin' }),
+        generateStringKey: 'password',
+        excludePunctuation: true,
+      },
+    });
+
+    // Instância RDS MySQL
+    const database = new rds.DatabaseInstance(this, 'DesafioRDS', {
+      engine: rds.DatabaseInstanceEngine.mysql({ version: rds.MysqlEngineVersion.VER_8_0 }),
+      vpc,
+      credentials: rds.Credentials.fromSecret(dbCredentialsSecret),
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO),
+      allocatedStorage: 20,
+      maxAllocatedStorage: 100,
+      multiAz: false,
+      publiclyAccessible: false,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+      },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      deletionProtection: false,
+      databaseName: 'desafiodb',
+    });
   }
 }
