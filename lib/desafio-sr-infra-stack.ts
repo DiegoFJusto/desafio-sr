f247ad0c2275ae10c46949f3fd28614c643d33dd
DiegoFJusto <diegofjusto@gmail.com>
2025-05-30 17:24:17 -0300

    feat: full infra stack

--- lib/desafio-sr-infra-stack.ts
+++ lib/desafio-sr-infra-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns';
 import * as rds from 'aws-cdk-lib/aws-rds';
 import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 
@@ -9,43 +10,65 @@ export class DesafioSrInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // VPC com 2 AZs
+    // VPC
     const vpc = new ec2.Vpc(this, 'DesafioVpc', {
       maxAzs: 2,
     });
 
-    // Cluster ECS
+    // ECS Cluster
     const cluster = new ecs.Cluster(this, 'DesafioCluster', {
       vpc,
-      clusterName: 'DesafioSrCluster',
     });
 
-    // Secret para armazenar credenciais do banco
-    const dbCredentialsSecret = new secretsmanager.Secret(this, 'DBCredentialsSecret', {
-      secretName: 'DesafioDBCredentials',
+    // Fargate Service com Load Balancer e Auto Scaling
+    const fargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'DesafioService', {
+      cluster,
+      cpu: 256,
+      memoryLimitMiB: 512,
+      desiredCount: 1,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromRegistry('public.ecr.aws/docker/library/node:18'),
+        containerPort: 3000,
+      },
+      publicLoadBalancer: true,
+    });
+
+    // Auto Scaling baseado em uso de CPU
+    const scalableTarget = fargateService.service.autoScaleTaskCount({
+      minCapacity: 1,
+      maxCapacity: 4,
+    });
+
+    scalableTarget.scaleOnCpuUtilization('CpuScaling', {
+      targetUtilizationPercent: 50,
+      scaleInCooldown: cdk.Duration.seconds(60),
+      scaleOutCooldown: cdk.Duration.seconds(60),
+    });
+
+    // Secret para o banco de dados
+    const dbSecret = new secretsmanager.Secret(this, 'DbSecret', {
       generateSecretString: {
         secretStringTemplate: JSON.stringify({ username: 'admin' }),
         generateStringKey: 'password',
-        excludePunctuation: true,
+        excludeCharacters: '"@/\\',
       },
     });
 
-    // Instância RDS MySQL
-    const database = new rds.DatabaseInstance(this, 'DesafioRDS', {
+    // Banco de dados RDS MySQL
+    const db = new rds.DatabaseInstance(this, 'DesafioDatabase', {
       engine: rds.DatabaseInstanceEngine.mysql({ version: rds.MysqlEngineVersion.VER_8_0 }),
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
       vpc,
-      credentials: rds.Credentials.fromSecret(dbCredentialsSecret),
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO),
+      credentials: rds.Credentials.fromSecret(dbSecret),
+      multiAz: false,
       allocatedStorage: 20,
       maxAllocatedStorage: 100,
-      multiAz: false,
       publiclyAccessible: false,
       vpcSubnets: {
         subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
       },
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       deletionProtection: false,
-      databaseName: 'desafiodb',
     });
   }
 }
